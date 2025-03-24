61900e37204a193f6a72a527631e0d921522307b
JDH <chebakov2009@gmail.com>
2025-03-24 22:58:15 +0300

    Тренировки в полёте

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-@app.route('/')
-@app.route('/index/<title>')
-def index(title='Миссия Колонизация Марса'):
-    return render_template('base.html', title=title)
+
+@app.route('/training/<prof>')
+def sim(prof='Врач'):
+    if 'инженер' in prof or 'строитель' in prof:
+        sim = 'Инженерные тренажеры'
+        image = '/static/image/Engineering.jpeg'
+    else:
+        sim = 'Научные симуляторы'
+        image = '/static/image/Scientific.jpeg'
+    return render_template('base.html', sim=sim, image=image)
 
 
 if __name__ == '__main__':
