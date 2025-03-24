039626d1159b09c9782d9a8504269f950879581e
JDH <chebakov2009@gmail.com>
2025-03-24 22:54:56 +0300

    Готовимся к миссии

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
-@app.route('/carousel')
-def mars():
-    return '''<!doctype html>
-                <html lang="ru">
-                <head>
-                    <meta charset="UTF-8">
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Пейзажи Porsche</title>
-                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-                </head>
-                <body>
-                    <div class="container mt-4">
-                        <h1 class="text-center">Пейзажи Porsche</h1>
-                        <div id="porscheCarousel" class="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img src="/static/image/porshe1.jpeg" class="d-block w-100" alt="porshe 1">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="/static/image/porshe2.jpeg" class="d-block w-100" alt="porshe 2">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="/static/image/porshe3.jpeg" class="d-block w-100" alt="porshe 3">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </body>
-                </html>'''
+@app.route('/')
+@app.route('/index/<title>')
+def index(title='Миссия Колонизация Марса'):
+    return render_template('base.html', title=title)
 
 
 if __name__ == '__main__':
