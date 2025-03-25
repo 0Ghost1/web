a2deb05ca784f4d39a688982df02254256864718
JDH <chebakov2009@gmail.com>
2025-03-25 22:20:38 +0300

    Список профессий

--- main.py
+++ main.py
@@ -2,17 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+prof = ['Инженер', 'Механик', "Конструктор", "Програмист"]
 
-
-@app.route('/training/<prof>')
-def sim(prof='Врач'):
-    if 'инженер' in prof or 'строитель' in prof:
-        sim = 'Инженерные тренажеры'
-        image = '/static/image/Engineering.jpeg'
-    else:
-        sim = 'Научные симуляторы'
-        image = '/static/image/Scientific.jpeg'
-    return render_template('base.html', sim=sim, image=image)
+@app.route('/list_prof/<param>')
+def proffes(param):
+    return render_template('base.html', prof=prof, param=param)
 
 
 if __name__ == '__main__':
