cd2598423eccb2bf073f6bdc3d1089012be00e5d
JDH <chebakov2009@gmail.com>
2025-03-27 23:19:06 +0300

    Добавляем капитана

---
+++ data/users.py
@@ -0,0 +1,31 @@
+import datetime
+import sqlalchemy
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String)
+    name = sqlalchemy.Column(sqlalchemy.String)
+    age = sqlalchemy.Column(sqlalchemy.Integer)
+    position = sqlalchemy.Column(sqlalchemy.String)
+    speciality = sqlalchemy.Column(sqlalchemy.String)
+    address = sqlalchemy.Column(sqlalchemy.String)
+    email = sqlalchemy.Column(sqlalchemy.String)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now())
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    job = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer, nullable=False)
+    collaborators = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    start_date = sqlalchemy.Column(sqlalchemy.String)
+    end_date = sqlalchemy.Column(sqlalchemy.String)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask
+from data import db_session
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-prof = ['Инженер', 'Механик', "Конструктор", "Програмист"]
 
-@app.route('/list_prof/<param>')
-def proffes(param):
-    return render_template('base.html', prof=prof, param=param)
+
+def main():
+    db_session.global_init("db/blogs.db")
+    user = [
+        User(surname='Scott', name='Ridley', age=21,
+             position='captain', speciality='research engineer',
+             address='module_1', email='scott_chief@mars.org'),
+
+        User(surname='Murkinez', name='Dai', age=16,
+             position='captain', speciality='ml engineer',
+             address='module_3', email='davmrkinez@mars.org'),
+
+        User(surname='Rock', name='Skala', age=99,
+             position='Rock', speciality='Real rock',
+             address='module_99', email='rock@mars.org')]
+
+    db_sess = db_session.create_session()
+    db_sess.add_all(user)
+    db_sess.commit()
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    main()
